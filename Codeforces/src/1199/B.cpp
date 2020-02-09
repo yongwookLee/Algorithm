6282e34c697a589304c9cffcda6c529e8fbe0270
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:12:16 +0900

    Add for 1199B Water Lily

---
+++ Codeforces/src/1199/B.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+    int H,L;
+    scanf("%d %d",&H,&L);
+    double ans;
+    ans+=(double)H/2;
+    ans+=(double)L/2/H*L;
+    printf("%.13lf",ans-H);
+}

effa01bdd32b9bd488b964f80fb3db2227c72534
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:14:27 +0900

    Add for 1183C Computer Game

---
+++ Codeforces/src/1183/C.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int q;
+    scanf("%d",&q);
+    for(int i=0;i<q;i++){
+        int n,k,a,b;
+        scanf("%d %d %d %d",&k,&n,&a,&b);
+        int r,q1,q2;
+        if(((k-1)/b)<n) printf("-1\n");
+        else{
+            printf("%d\n",((k-n*b-1)/(a-b)>n)?n:(k-n*b-1)/(a-b));
+        }
+    }
+}

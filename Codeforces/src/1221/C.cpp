416086c46db71917c2ea649fd4fd5aed58f4afd1
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:10:07 +0900

    Add for 1221C Perfect Team

---
+++ Codeforces/src/1221/C.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int c,m,x;
+        scanf("%d %d %d",&c,&m,&x);
+        int mini=std::min(c,m);
+        c-=mini;
+        m-=mini;
+            if(c+m+x>=mini){
+                printf("%d\n",mini);
+            }
+            else    printf("%d\n",mini-(mini-c-m-x+2)/3);
+    }
+}

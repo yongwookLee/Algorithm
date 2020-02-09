4fef8da4a48d27d25721fee86a259354d46a00aa
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:15:38 +0900

    Add for 1107B Digital root

---
+++ Codeforces/src/1107/B.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int n,t;
+	__int64 ans=0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		__int64 k;
+		scanf("%I64dd",&k);
+		scanf("%d",&t);
+		ans=9*(k-1)+t;
+		printf("%I64d\n",ans);
+		ans=0;
+	}
+
+}

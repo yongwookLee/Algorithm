0f02d6f672ce495dec99bc8da771ec3b6978d44e
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:08:52 +0900

    Add for 1228B Filling the Grid

---
+++ Codeforces/src/1228/B.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int cell[1005][1005];
+
+int main(){
+    int h,w;
+    scanf("%d %d",&h,&w);
+    for(int i=0;i<h;i++){
+        int tmp;
+        scanf("%d",&tmp);
+        for(int j=0;j<tmp;j++){
+            cell[i][j]=1;
+        }
+        cell[i][tmp]=-1;
+    }
+    int zero=0;
+    for(int i=0;i<w;i++){
+        int tmp;
+        scanf("%d",&tmp);
+        for(int j=0;j<tmp;j++){
+            if(cell[j][i]==-1){
+                zero=1;
+            }
+            else{
+                cell[j][i]=1;
+            }
+        }
+        if(cell[tmp][i]==1) zero=1;
+        else    cell[tmp][i]=-1;
+    }
+    if(zero){
+        printf("0");
+        return 0;
+    }
+    int cnt=0;
+    for(int i=0;i<h;i++)    for(int j=0;j<w;j++){
+        if(cell[i][j]==0)   cnt++;
+    }
+    long long ans=1;
+    for(int i=0;i<cnt;i++){
+        ans*=2;
+        if(ans>=1000000007)
+            ans%=1000000007;
+    }
+    printf("%d",ans);
+}

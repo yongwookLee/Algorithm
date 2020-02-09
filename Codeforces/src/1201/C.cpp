6f554615943e6ffabf457ca5f8c572820cb3dc0e
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:11:40 +0900

    Add for 1201C Maximum Median

---
+++ Codeforces/src/1201/C.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <functional>
+using namespace std;
+int a[200001];
+int main(){
+    long long n,k;
+    scanf("%lld %lld",&n,&k);
+    for(int i=0;i<n;i++)
+        scanf("%d",a+i);
+    sort(a,a+n);
+    int i;
+    for(i=n/2;i<n-1;i++){
+        if((long long)(a[i+1]-a[i])*(i-n/2+1)<=k)
+            k-=(long long)(a[i+1]-a[i])*(i-n/2+1);
+        else break;
+    }
+    if(k>0) a[i]+=k/(i-n/2+1);
+    printf("%d",a[i]);
+}

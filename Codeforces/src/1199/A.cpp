f0b5b91c280bc8d1bdc8f24072c1a345eeef633e
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:12:24 +0900

    Add for 1199A City Day

---
+++ Codeforces/src/1199/A.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int arr[100001];
+int main(){
+    int n,x,y;
+    scanf("%d %d %d",&n,&x,&y);
+    for(int i=0;i<n;i++){
+        scanf("%d",arr+i);
+    }
+    for(int i=0;i<n;i++){
+            int f=0;
+        for(int j=i-x;j<=i+y;j++){
+            if(j<0 || j>=n || j==i) continue;
+            if(arr[j]<arr[i]){
+                f=1;break;
+            }
+        }
+        if(!f){
+            printf("%d",i+1);
+            break;
+        }
+
+    }
+
+}

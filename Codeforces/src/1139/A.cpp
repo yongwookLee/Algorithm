11fd8db35fbd9b39a1645234f67b205649cc80e5
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:15:02 +0900

    Add for 1139A Even Substrings

---
+++ Codeforces/src/1139/A.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+char a[65001];
+int main(){
+    int n,sum=0;
+    scanf("%d",&n);
+    scanf("%s",a);
+    for(int i=1;i<=n;i++){
+        if(int(a[i-1]-'0')%2==0){
+            sum+=i;
+        }
+    }
+    printf("%d",sum);
+}

aa4967460a96dba16d83b9b8fc081220c2558c90
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:17:42 +0900

    Add for 1097A Gennady and a Card Game

---
+++ Codeforces/src/1097/A.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	char Hand[5][2];
+	char Table[2];
+	scanf("%s",	Table);
+	for(int i=0;i<5;i++)
+		scanf("%s",Hand[i]);
+	for(int i=0;i<5;i++){
+		if(Hand[i][0]==Table[0]){
+			printf("YES");
+			return 0;
+		}
+		if(Hand[i][1]==Table[1]){
+			printf("YES");
+			return 0;
+		}
+	}
+	printf("NO");
+	return 0;
+}

6b3ea05451c107cb1896bf1e84bad96146c59447
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:12:44 +0900

    Add for 1178B WOW Factor

---
+++ Codeforces/src/1178/B.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+char s[1000005];
+int w[1000005];
+int cnt[1000005];
+int main(){
+    scanf("%s",s);
+    int len=strlen(s);
+    for(int i=0;i<len-1;i++){
+        if(s[i]=='v'&&s[i+1]=='v')
+            w[i]=1;
+    }
+    cnt[0]=w[0];
+    for(int i=1;i<len;i++){
+        cnt[i]=cnt[i-1]+w[i];
+    }
+    long long sum=0;
+    for(int i=1;i<len;i++){
+        if(s[i]=='o'){
+            sum+=((long long)cnt[i-1])*(cnt[len-1]-cnt[i]);
+        }
+    }
+    printf("%lld",sum);
+}

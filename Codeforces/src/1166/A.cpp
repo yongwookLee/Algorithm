6c87e09c76cca9ab4814c650c9dc564cec763c98
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:13:53 +0900

    Add for 1166A Silent Classroom

---
+++ Codeforces/src/1166/A.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int alph[30];
+
+int f(int n){
+    return n*(n-1)/2;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        char tmp[21];
+        scanf("%s",tmp);
+        alph[tmp[0]-'a']++;
+    }
+    int sum=0;
+    for(int i=0;i<26;i++){
+        sum+=(f(alph[i]/2)+f(alph[i]-alph[i]/2));
+    }
+    printf("%d",sum);
+}

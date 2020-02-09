1d1194fb1d83514f3426a5914395e860265c636e
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:08:04 +0900

    Add for 1294A Collecting Coins

---
+++ Codeforces/src/1294/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,ans=0;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int a,b,c,n;
+        scanf("%d %d %d %d",&a,&b,&c,&n);
+        if((a+b+c+n)%3==0){
+            int t=(a+b+c+n)/3;
+            if(a<=t)if(b<=t)if(c<=t){
+                printf("YES\n");
+                continue;
+            }
+        }
+        printf("NO\n");
+    }
+}

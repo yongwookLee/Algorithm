e086595379d258a48c999a2615266bb6c8515689
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:16:48 +0900

    Add for 1100C NN and the Optical Illusion

---
+++ Codeforces/src/1100/C.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.1415926535
+
+int main(){
+	int n,r;
+	scanf("%d %d",&n,&r);
+	double nd, rd;
+	nd=(double)n;
+	rd=(double)r;
+	double precal=1/(1-(sin(PI/nd)));
+	printf("%lf",precal*rd-rd);
+}

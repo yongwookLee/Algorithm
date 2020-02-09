68c888f6358367d5e4e65a2b288c06a142657d2b
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:07:09 +0900

    Add for 1300B Assigning to Classes

---
+++ Codeforces/src/1300/B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,q,t,n,m;
+    cin>>q;
+    while(q--){
+        scanf("%d",&n);
+        int a[200005];
+        for(int i=0;i<2*n;i++)
+            scanf("%d",a+i);
+        sort(a,a+2*n);
+        printf("%d\n",a[n]-a[n-1]);
+    }
+}

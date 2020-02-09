638bf767ddf3ebe5fba27445f1a9d6f3580c0864
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:16:28 +0900

    Add for 1104B Game with string

---
+++ Codeforces/src/1104/B.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stack>
+
+using namespace std;
+char str[100001];
+stack<char> s;
+
+int main(){
+	scanf("%s",str);
+	int count=0;
+	int len=strlen(str);
+	for(int i=0;i<len;i++){
+		if(s.empty()){
+			s.push(str[i]);
+			continue;
+		}
+		if(s.top()==str[i]){
+			s.pop();
+			count++;
+		}
+		else{
+			s.push(str[i]);
+		}
+	}
+	if(count%2)	printf("Yes");
+	else	printf("No");
+}

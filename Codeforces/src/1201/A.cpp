e532121c111feab37759e401c34ef26bbffbdcd9
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:11:55 +0900

    Add for 1201A Important Exam

---
+++ Codeforces/src/1201/A.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <functional>
+using namespace std;
+
+char a[1000][1001];
+int ans[1001];
+int cnt[1001][50];
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<n;i++)
+        scanf("%s",a[i]);
+    for(int i=0;i<m;i++)
+        scanf("%d",ans+i);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cnt[j][a[i][j]-'A']++;
+        }
+    }
+    for(int j=0;j<m;j++)
+        sort(cnt[j],cnt[j]+50,greater<int>());
+    int qe=0;
+    for(int i=0;i<m;i++)
+        qe+=(cnt[i][0]*ans[i]);
+    printf("%d",qe);
+}

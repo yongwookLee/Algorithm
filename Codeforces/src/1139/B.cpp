ec905ec4f812347d47afd9815ef80b0c96d4bc35
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:14:55 +0900

    Add for 1139B Chocolates

---
+++ Codeforces/src/1139/B.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int a[200001];
+
+int main(){
+    int n;
+    long long sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",a+i);
+    }
+    sum+=a[n-1];
+    for(int i=n-2;i>=0;i--){
+        if(a[i]>=a[i+1]){
+            if(a[i+1]<=1)   a[i]=0;
+            else    a[i]=a[i+1]-1;
+            sum+=a[i];
+        }
+        else    sum+=a[i];
+    }
+    printf("%lld",sum);
+}

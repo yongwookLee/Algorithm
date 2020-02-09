9614bd94c139c0c6a913537f7ca266793d75b27d
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:17:49 +0900

    Add for 1095A Repeating Cipher

---
+++ Codeforces/src/1095/A.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int i;
+int main(){
+    int N;
+    char str[55];
+    scanf("%d",&N);
+    scanf("%s",str);
+    for(int j=0;j<N;j+=i){
+    	printf("%c",str[j]);
+    	i++;
+	}
+}

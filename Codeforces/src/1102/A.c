2a3133e7096609d8aec0f8f63782f26fd65f63c0
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:17:28 +0900

    Add for 1102A Integer Sequence Dividing

---
+++ Codeforces/src/1102/A.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	if(n%2){
+		if((n-1)%4==0){
+			printf("1");
+		}
+		else{
+			printf("0");
+		}
+	}
+	else{
+		if(n%4==0){
+			printf("0");
+		}
+		else{
+			printf("1");
+		}
+	}
+}

5c5741900e6775e03c892bc5e12e62fcdec18018
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:17:15 +0900

    Add for 1102C Doors Breaking and Repairing

---
+++ Codeforces/src/1102/C.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int n,x,y;
+int doorDur[101];
+int main(){
+	scanf("%d %d %d",&n,&x,&y);
+	for(int i=0;i<n;i++)
+		scanf("%d",&doorDur[i]);
+	if(x>y){
+		printf("%d",n);
+		return 0;
+	}
+	if(x<=y){
+		int weak=0, strong=0;
+		for(int i=0;i<n;i++){
+			if(doorDur[i]<=x)	weak++;
+			else	strong++;
+		}
+		printf("%d",weak/2+weak%2);
+	}
+}

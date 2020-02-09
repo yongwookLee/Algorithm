4bff4dfe42faabd8ed5c045caad68ea28a51207b
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:13:39 +0900

    Add for 1189A Keanu Reeves

---
+++ Codeforces/src/1189/A.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+char a[101];
+int main(){
+    int n;
+    scanf("%d",&n);
+    scanf("%s",a);
+
+    int one=0,zero=0;
+    for(int i=0;i<n;i++){
+        if(a[i]=='0')   zero++;
+        else one++;
+    }
+    if(one==zero){
+        printf("2\n%c %s",a[0],a+1);
+    }
+    else{
+        printf("1\n%s",a);
+    }
+}

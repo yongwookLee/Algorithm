b18dd4606f2ba9b81ac77d39133c0992eabfccea
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:11:02 +0900

    Add for 1204A BowWow and the Timetable

---
+++ Codeforces/src/1204/A.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+int b[60];
+int main(){
+    char s[110];
+    scanf("%s",&s);
+    int len=strlen(s);
+    if(len%2==1){
+        for(int i=1;i<len;i++){
+            if(s[i]=='1'){
+                printf("%d",len/2+1);
+                return 0;
+            }
+        }
+        printf("%d",len/2);
+        return 0;
+    }
+    else   printf("%d",len/2);
+}

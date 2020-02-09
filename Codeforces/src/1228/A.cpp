ad333418b491509fa625e2df1d3cc12d69bcd039
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:08:58 +0900

    Add for 1228A Distinct Digits

---
+++ Codeforces/src/1228/A.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int dist(int n){
+    int a[10];
+    for(int i=0;i<10;i++){
+        a[i]=0;
+    }
+    while(n){
+        if(a[n%10]) return 0;
+        else{
+            a[n%10]++;
+            n/=10;
+        }
+    }
+    return 1;
+}
+int main(){
+    int l,r;
+    scanf("%d %d",&l,&r);
+    for(int i=l;i<=r;i++){
+        if(dist(i)){
+            printf("%d",i);
+            return 0;
+        }
+    }
+    printf("-1");
+    return 0;
+}

d711f0754a1fa45d565dc75d449a17243a8d626a
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:14:41 +0900

    Add for 1183A Nearest Interesting Number

---
+++ Codeforces/src/1183/A.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int largeInter(int a){
+    int sum=0;
+    for(int i=a;;sum=0,i++){
+        int tmp=i;
+        while(tmp){
+            sum+=(tmp%10);
+            tmp/=10;
+        }
+        if(sum%4==0)    return i;
+    }
+}
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d",largeInter(a));
+}

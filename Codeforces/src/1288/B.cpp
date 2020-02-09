878d6f70eed0642e4c6a4f645af17ad1a1215da7
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:08:25 +0900

    Add for 1288B Yet Another Meme Problem

---
+++ Codeforces/src/1288/B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int A,B;
+        cin>>A>>B;
+        int b=9;
+        long long cnt=0;
+        for(b=9;b<=B;b=b*10+9){
+            cnt+=A;
+            if(b==999999999)    break;
+        }
+        cout<<cnt<<endl;
+    }
+
+}

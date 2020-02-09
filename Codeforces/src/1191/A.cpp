34362976dbfc0deabb167a779066b073fc6f90e2
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:13:12 +0900

    Add for 1191A Tokitsukaze and Enhancement

---
+++ Codeforces/src/1191/A.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%4==0)  printf("1 A");
+    else if(n%4==1)  printf("0 A");
+    else if(n%4==2)  printf("1 B");
+    else printf("2 A");
+
+}

99715d153234fea3a2591179dc557d03510367eb
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:09:40 +0900

    Add for 1216B Shooting

---
+++ Codeforces/src/1216/B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int> a[1010];
+int main(){
+    int n,m,b,c,p,q;
+    long long cnt=0,sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int tmp;
+        scanf("%d",&tmp);
+        a[i]=make_pair(tmp,i+1);
+    }
+    sort(a,a+n,greater<pair<int,int> >());
+    for(int i=0;i<n;i++){
+        sum+=a[i].first*i+1;
+    }
+    printf("%lld\n",sum);
+    for(int i=0;i<n;i++){
+        printf("%lld ",a[i].second);
+    }
+
+}

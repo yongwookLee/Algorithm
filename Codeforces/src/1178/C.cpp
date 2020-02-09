9db11f85f23bb183e8d388d0f8f2e44d64a3352f
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:12:37 +0900

    Add for 1178C Tiles

---
+++ Codeforces/src/1178/C.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int w,h;
+    scanf("%d %d",&w,&h);
+    int n=4;
+    for(int i=1;i<w;i++){
+        n*=2;
+        n%=998244353;
+    }
+    for(int i=1;i<h;i++){
+        n*=2;
+        n%=998244353;
+    }
+    printf("%d",n);
+}

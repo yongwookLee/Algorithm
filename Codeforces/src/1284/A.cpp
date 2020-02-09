994dcb97c2b540355574f5e016dacd5028d9cc78
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:08:45 +0900

    Add for 1284A New Year and Naming

---
+++ Codeforces/src/1284/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char s[21][11];
+    char t[21][11];
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+        cin>>s[i];
+    for(int i=0;i<m;i++)
+        cin>>t[i];
+    int q;
+    scanf("%d",&q);
+    for(int i=0;i<q;i++){
+        int y;
+        scanf("%d",&y);
+        printf("%s%s\n",s[(y-1)%n],t[(y-1)%m]);
+    }
+
+}

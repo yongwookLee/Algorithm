18322885f85fd1a4172e437693c24afd1e785a0d
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:09:19 +0900

    Add for 1230A Dawid and Bags of Candies

---
+++ Codeforces/src/1230/A.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+ 
+    int cnt=0,ans=0,sum=0;
+    sum=a+b+c+d;
+        if(a+b==c+d||a+c==b+d||a+d==b+c||a==b+c+d||b==a+c+d||c==b+a+d||d==b+c+a)  printf("YES");
+        else    printf("NO");
+    
+}

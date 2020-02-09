b45c3ef3d5daa1d05c687eb75bfecd4828dbd43e
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:14:00 +0900

    Add for 1185A Ropewalkers

---
+++ Codeforces/src/1185/A.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<algorithm>
+
+int main(){
+    int a[3],d;
+    scanf("%d %d %d %d",a,a+1,a+2,&d);
+    std::sort(a,a+3);
+    if(a[1]-a[0]>=d && a[2]-a[1]>=d)
+        printf("0");
+    else if(a[1]-a[0]<d && a[2]-a[1]>=d)
+        printf("%d",d-a[1]+a[0]);
+    else if(a[2]-a[1]<d && a[1]-a[0]>=d)
+        printf("%d",d-a[2]+a[1]);
+    else
+        printf("%d",2*d-a[2]+a[0]);
+}

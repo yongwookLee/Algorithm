f377cf1e1b09de243e6a4b7e4ab75193fda0e727
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:13:19 +0900

    Add for 1189D1 Add on a Tree

---
+++ Codeforces/src/1189/D1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int a[100001];
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n-1;i++){
+        int tmp1,tmp2;
+        scanf("%d %d",&tmp1,&tmp2);
+        a[tmp1]++,a[tmp2]++;
+    }
+    for(int i=1;i<=n;i++){
+        if(a[i]==2){
+            printf("NO");
+            return 0;
+        }
+    }
+    printf("YES");
+}

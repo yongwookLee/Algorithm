de37db1cfe95fad3cfd43a38e9b20ef8fd4bb4d7
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:16:55 +0900

    Add for 1100B Build a Contest

---
+++ Codeforces/src/1100/B.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int prob[100010];
+int visited[100010];
+int main(){
+	int n,m,check;
+	scanf("%d %d",&n,&m);
+	for(int i=0;i<m;i++){
+		scanf("%d",prob+i);
+	}
+	check=n;
+	for(int i=0;i<m;i++){
+		if(visited[prob[i]]==0){
+			visited[prob[i]]=1;
+			check--;
+		}
+		else{
+			visited[prob[i]]++;
+		}
+		if(check==0){
+			printf("1");
+			check=n;
+			for(int j=1;j<=n;j++)	visited[j]--;
+			for(int j=1;j<=n;j++)	if(visited[j])	check--;
+		}
+		else	printf("0");
+	}
+}

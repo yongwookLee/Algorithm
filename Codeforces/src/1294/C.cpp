6f5ce6f33ed91f4ec609f0496c86cca7e26b65a9
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:07:56 +0900

    Add for 1294C Product of Three Numbers

---
+++ Codeforces/src/1294/C.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,ans=0;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        scanf("%d",&n);
+        int a=0,b=0;
+        for(int i=2;i<sqrt(n);i++){
+            if(n%i==0){
+                if(a){
+                    b=i;
+                    n/=b;
+                    break;
+                }
+                else{
+                    a=i;
+                    n/=i;
+                }
+            }
+        }
+        if(b){
+            printf("YES\n%d %d %d\n",a,b,n);
+        }
+        else    printf("NO\n");
+    }
+}

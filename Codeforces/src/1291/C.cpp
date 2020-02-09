9c849b69b930e51cae876fd604e5f8b706dc3919
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:07:26 +0900

    Add for 1291C Mind Control

---
+++ Codeforces/src/1291/C.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(int q=0;q<t;q++){
+        int n,m,k;
+        scanf("%d %d %d",&n,&m,&k);
+        int maxi=0;
+        int a[3600];
+        for(int i=0;i<n;i++){
+            scanf("%d",a+i);
+        }
+        if(m-1-k<=0){
+            for(int i=0;i<m;i++){
+                if(maxi<a[i])   maxi=a[i];
+            }
+            for(int i=n-1;i>n-m-1;i--){
+                if(maxi<a[i])   maxi=a[i];
+            }
+            printf("%d\n",maxi);
+            continue;
+        }
+
+        for(int t=0;t<=k;t++){
+            int mini=1e9;
+            for(int i=t,j=n-m+t;i<=m-k-1+t;i++,j++){
+                int tmp=max(a[i],a[j]);
+                if(mini>tmp)   mini=tmp;
+            }
+            if(mini>maxi)   maxi=mini;
+        }
+        printf("%d\n",maxi);
+    }
+}

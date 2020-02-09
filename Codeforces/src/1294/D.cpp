8f066bda9c37536eafc1d549c8af52070761aa25
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:07:49 +0900

    Add for 1294D MEX maximizing

---
+++ Codeforces/src/1294/D.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[400000];
+int main(){
+    int q,x;
+    cin>>q>>x;
+    int mex=0;
+    int cnt=0;
+    for(int i=0;i<q;i++){
+        int n;
+        scanf("%d",&n);
+        a[n%x]++;
+        for(;;mex++){
+            if(mex==x){
+                mex=0;
+                cnt++;
+            }
+            if(a[mex]){
+                a[mex]--;
+            }
+            else    break;
+        }
+        printf("%d\n",mex+cnt*x);
+    }
+}

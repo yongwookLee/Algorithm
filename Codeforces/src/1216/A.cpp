fc9bef178b0d60935750b6e2f4701da334093349
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:09:46 +0900

    Add for 1216A Prefixes

---
+++ Codeforces/src/1216/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int n,m,a,b,c,p,q;
+    int cnt=0,sum=0;
+    char s[200005];
+    scanf("%d",&n);
+    scanf("%s",&s);
+    for(int i=0;i<n;i+=2){
+        if(s[i]!=s[i+1])    continue;
+        else{
+            if(s[i]=='a')   s[i]='b';
+            else    s[i]='a';
+            cnt++;
+        }
+    }
+    printf("%d\n",cnt);
+    printf("%s",s);
+}

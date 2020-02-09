85e5ca17a0b9407f898a3b11f7ec0717ce317692
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:15:09 +0900

    Add for 1121B Mike and Children

---
+++ Codeforces/src/1121/B.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<algorithm>
+#include<functional>
+
+int count[200001];
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<(n);i++){
+        scanf("%d",a+i);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==i)
+                continue;
+            else{
+                count[a[i]+a[j]]++;
+            }
+        }
+    }
+    std::sort(count,count+200000,std::greater<int>());
+    printf("%d",count[0]/2);
+}

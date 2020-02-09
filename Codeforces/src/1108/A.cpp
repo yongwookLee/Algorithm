91ce42dc302326936b6c1f1df278325e00e2ab2f
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:16:14 +0900

    Add for 1108A Two distinct points

---
+++ Codeforces/src/1108/A.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int arr[4];
+int main(){
+	int q;
+	scanf("%d",&q);
+	for(int i=0;i<q;i++){
+		scanf("%d %d %d %d",arr,arr+1,arr+2,arr+3);
+		if(arr[0]==arr[2]){
+			printf("%d %d\n",arr[0],arr[0]+1);
+		}
+		else{
+			printf("%d %d\n",arr[0],arr[2]);
+		}
+	}
+
+}

924da96367947c50441572b9d52c3a1e19df7694
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:14:35 +0900

    Add for 1183B Equalize Prices

---
+++ Codeforces/src/1183/B.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int q;
+    scanf("%d",&q);
+    for(int i=0;i<q;i++){
+        int n,k;
+        scanf("%d %d",&n,&k);
+        int mini=100000000;
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            int tmp;
+            scanf("%d",&tmp);
+            if(mini>tmp)    mini=tmp;
+            if(maxi<tmp)    maxi=tmp;
+        }
+        if(maxi-mini<=2*k){
+            printf("%d\n",mini+k);
+        }
+        else printf("-1\n");
+    }
+}

5c8a41b0472f134c45ebd194940d75dd76ac7da0
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:10:34 +0900

    Add for 1215A Yellow Cards

---
+++ Codeforces/src/1215/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a1,a2,k1,k2,n;
+    int maxn=0,minn=0;
+    scanf("%d %d %d %d %d",&a1,&a2,&k1,&k2,&n);
+    if(k1<k2){
+        if(a1*k1<=n)    maxn=a1+(n-a1*k1)/k2;
+        else    maxn=n/k1;
+    }
+    else{
+        if(a2*k2<=n)    maxn=a2+(n-a2*k2)/k1;
+        else    maxn=n/k2;
+    }
+    minn=(a1+a2)-(a1*k1+a2*k2)+n;
+    if(minn<0)  minn=0;
+    printf("%d %d",minn, maxn);
+}

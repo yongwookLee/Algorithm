ed83d66b33de55dd37924fcdb694f9b8b52f8486
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:07:18 +0900

    Add for 1300A Non-zero

---
+++ Codeforces/src/1300/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,q,t,n,m;
+    cin>>q;
+    while(q--){
+        scanf("%d",&n);
+        int a[110];
+        for(int i=0;i<n;i++)
+            scanf("%d",a+i);
+        int sum=0,cnt=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                a[i]++;
+                cnt++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        if(sum) printf("%d\n",cnt);
+        else    printf("%d\n",cnt+1);
+    }
+}

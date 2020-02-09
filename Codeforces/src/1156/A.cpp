f219e2f5a6a93ee511d499e6afb2ff7b22b4eb08
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:14:48 +0900

    Add for 1156A Inscribed Figures

---
+++ Codeforces/src/1156/A.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int what[101];
+int main(){
+    int n,ans=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)    scanf("%d",what+i);
+    for(int i=0;i<n-1;i++){
+        switch(what[i]){
+        case 1:
+            switch(what[i+1]){
+            case 1:
+                ans+=10000;
+                break;
+            case 2:
+                ans+=3;
+                if(i>=1)    if(what[i-1]==3)    ans--;   
+                break;
+            case 3:
+                ans+=4;
+                break;
+            }
+            break;
+        case 2:
+            switch(what[i+1]){
+            case 1:
+                ans+=3;
+                break;
+            case 2:
+                ans+=10000;
+                break;
+            case 3:
+                ans+=10000;
+                break;
+            }
+            break;
+        case 3:
+            switch(what[i+1]){
+            case 1:
+                ans+=4;
+                break;
+            case 2:
+                ans+=10000;
+                break;
+            case 3:
+                ans+=10000;
+                break;
+            }
+            break;
+        }
+    }
+    if(ans>=10000){
+        printf("Infinite\n");
+    }
+    else{
+        printf("Finite\n%d",ans);
+    }
+}

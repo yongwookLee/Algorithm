1f6e84429318164d6d967d132f855d45ca28b0ae
yongwookLee <yongw00k@g.skku.edu>
2020-02-10 04:15:45 +0900

    Add for 1107A Digits Sequence Dividing

---
+++ Codeforces/src/1107/A.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char str[301];
+int main(){
+	int q,n;
+	scanf("%d",&q);
+	for(int i=0;i<q;i++){
+		scanf("%d",&n);
+		scanf("%s",str);
+		if(n>2){
+			printf("YES\n2\n");
+			printf("%c %s\n",str[0],str+1);
+		}
+		else{
+			if(str[0]<str[1]){
+				printf("YES\n2\n");
+				printf("%c %c\n",str[0],str[1]);
+			}
+			else{
+				printf("NO\n");
+			}
+		}
+	}
+
+}

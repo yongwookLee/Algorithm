577064eff727a4b7cd7ca185f8b1bd025ef57cd8
yongwookLee <yongw00k@g.skku.edu>
2020-04-16 12:38:37 +0900

    Add for 1337A Ichihime and Triangle

---
+++ Codeforces/src/1337/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define mp make_pair
+#define f first
+#define s second
+
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
